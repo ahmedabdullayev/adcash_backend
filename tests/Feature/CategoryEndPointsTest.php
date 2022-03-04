5b01a1c959ba14f1e6ba185583d14279f25358ec
ahmedabdullayev <ahmed.abdull2016@gmail.com>
2022-03-04 17:23:36 +0200

    tests added + removed useless files

---
+++ tests/Feature/CategoryEndPointsTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class CategoryEndPointsTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_get_categories()
+    {
+        $response = $this->get('/api/categories');
+
+        $response->assertStatus(200);
+    }
+    public function test_post_category()
+    {
+        $response = $this->post('/api/category', ['name' => 'TestCategory']);
+
+        $response->assertStatus(200);
+    }
+    public function test_delete_category()
+    {
+        $response = $this->delete('/api/category/19');
+
+        $response->assertStatus(200);
+    }
+
+
+}
